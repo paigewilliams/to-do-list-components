efd0d662c862b136fb032133ec6f6771409f4f87
Charley McGowan and Sheila Stephen <charley@>
2019-01-22 10:11:55 -0800

    Do the TODO LIST in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model'
 
 @Component({
   selector: 'crazy-app-selector',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  {
   currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDay();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+  new Task('Finish weekend Angular homework for Epicodus course'),
+  new Task('Begin brainstorming possible JavaScript group projects'),
+  new Task('Add README file to last few Angular repos on GitHub')
+  ];
 }
