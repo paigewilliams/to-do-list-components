d05fcf24d8173b420951395f87b0bbefd8664961
Paige Williams and Sheila Stephen <sheila@>
2019-01-23 13:21:44 -0800

    Add event emitter for edit function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,15 @@ export class AppComponent  {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDay();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
+  selectedTask: Task = null;
+
+  masterTaskList: Task[] = [
   new Task('Finish weekend Angular homework for Epicodus course', 3),
   new Task('Begin brainstorming possible JavaScript group projects', 2),
   new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: Task = null;
-
   editTask(clickedTask){
     this.selectedTask = clickedTask;
-  }
-
-  priorityColor(currentTask) {
-    if (currentTask.priority == 3){
-      return "bg-danger";
-    } else if (currentTask.priority == 2) {
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
   }
 
   finishedEditing(){
